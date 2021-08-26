cb08afcc855390c20cc53264168a6e9397ece0f6
sebaegana <sebaegana@gmail.com>
2021-08-25 20:49:00 -0400

    Seg

--- code_sitio.R
+++ code_sitio.R
@@ -8,3 +8,14 @@ new_site(theme = "wowchemy/starter-academic")
 
 blogdown::serve_site()
 blogdown:::preview_site()
+
+blogdown::new_post(title = "Hi Hugo", 
+                   ext = '.Rmarkdown', 
+                   subdir = "post")
+blogdown::config_Rprofile() 
+
+file.edit(".gitignore")
+
+blogdown::check_gitignore()
+
+blogdown::check_content()
