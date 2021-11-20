54d70d598d93da46953d0fa495b6f0d2258699c9
sebaegana <sebaegana@gmail.com>
2021-11-20 00:39:20 -0300

    Update 20211120

---
+++ code_sitio.R
@@ -0,0 +1,36 @@
+if (!requireNamespace("remotes")) install.packages("remotes")
+library(blogdown)
+
+#remotes::install_github("rstudio/blogdown")
+# new_site(theme = "wowchemy/starter-academic")
+
+blogdown::serve_site()
+blogdown:::preview_site()
+
+stop_server()
+
+blogdown::new_post(title = "Hi Hugo", 
+                   ext = '.Rmarkdown', 
+                   subdir = "post")
+
+# blogdown::config_Rprofile() 
+# 
+# file.edit(".gitignore")
+# 
+# blogdown::check_gitignore()
+# 
+# blogdown::check_content()
+# 
+# rstudioapi::navigateToFile("config.yaml", line = 3)
+# 
+# 
+# blogdown::config_netlify()
+# 
+# blogdown::check_hugo()
+# 
+# rstudioapi::navigateToFile("config/_default/params.toml")
+# 
+# rstudioapi::navigateToFile("config/_default/menus.toml")
+# 
+# rstudioapi::navigateToFile("config.yaml", line = 15)
+
---
+++ content/courses/intro_r/_index.md
@@ -0,0 +1,17 @@
+---
+date: "2021-01-24"
+linkTitle: Data Science
+summary: An example of using Wowchemy's Book layout for publishing online courses.
+title: "\U0001F4CA Introducción a la programación en R"
+type: book
+---
+
+{{< figure src="intro_r.png" >}}
+
+{{< toc hide_on="xl" >}}
+
+## Enlaces del curso
+
+Página del curso en Notion:[Enlace acá](https://segana.notion.site/Curso-Introductorio-a-la-programaci-n-en-R-3481b6f07adb41a08f1958058ea8850e)
+
+
